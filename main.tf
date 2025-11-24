2135681744504e57c93c4194672c1bee12ba7998
CloudtechShankar <shankaryalagandula2004@gmail.com>
2025-11-24 22:16:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public-1" {
 
 resource "aws_instance" "name" {
     ami = "ami-0fa3fe0fa7920f68e"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     tags = {
       Name = "TF-EC2"
     }
